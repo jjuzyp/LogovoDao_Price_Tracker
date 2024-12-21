6741628ad11fe2f2aac2dc9ee06637f906a7c22d
jjuzyp <jjuzyp@gmail.com>
2024-12-21 21:46:49 +0300

    v2 first

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # LogovoDao_Price_Tracker
 BOT guide - jjuzyp.gitbook.io/logovopricetrackerbot-guide
-# installation guide
-Install dependencies
-```npm install```,
-create .env ```nano .env``` with ```TELEGRAM_TOKEN=``` ,
-run ```node main.js```.
 
-Price tracker telegram bot made by t.me/Logovo_DAO, t.me/AXAXAXAXAXAXAXAXAXAXAXXAXAXAA
+# installation guide
+Node version - v22.11.0, npm version - 10.9.0
+
+# GUIDE RU
+### Убедитесь что у вас установлен nodejs и npm!
+Скачайте архив и разархивируйте в любую папку. Откройте командную строку или редактор кода с терминалом и перейдите в папку с чеккером(cd C:\ваш путь\) ,установите зависимости командой ```npm install```. Заполните файл wallet.txt вашими кошельками(каждый с новой строки) и запустите командой ```node main.js```. 
+
+# GUIDE EN
+### Make sure you have nodejs and npm installed!
+Download the archive and unzip it to any folder. Open CMD or code editor with terminal and go to the folder with checker(cd C:\your path\), install dependencies with ```npm install```. Fill the wallet.txt file with your wallets (each with a new line) and run the code with the ``node main.js`` command.
+
+# CEDITS
+By t.me/AXAXAXAXAXAXAXAXAXAXAXXAXAXAA
+
+t.me/Logovo_DAO
--- main.js
+++ main.js
@@ -4,12 +4,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const telegramToken = process.env.TELEGRAM_TOKEN;
-
+const RPC = process.env.RPC;
 const bot = new TelegramBot(telegramToken, { polling: true });
 
+bot.on("polling_error", (error) => {
+    console.error("Polling error:", error);
+});
+
 let circulatingSupply = 0;
-let tasks = []; 
-let waitingForInput = false;
 let userTasks = {};
 
 async function fetchTokenSymbol(tokenAddress) {
@@ -19,108 +21,259 @@ async function fetchTokenSymbol(tokenAddress) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data = await response.json();
-        return data.pairs[0].baseToken.symbol; // Возвращаем символ токена
+        return data.pairs[0].baseToken.symbol; // returning token symbol
     } catch (error) {
         console.error('Error fetching token symbol:', error);
         return null;
     }
 }
 
-function createMainMenu() {
+
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Wellcome to LogovoPriceTrackerBot, this bot tracks solana token price changes! For more information use "Help" button! Choose your option:', createInlineMenu());
+});
+
+
+function createInlineMenu() {
     return {
         reply_markup: {
-            keyboard: [
-                [{ text: 'Add task' }],
-                [{ text: 'Task list' }, { text: 'Delete task' }],
-                [{ text: 'Delete all tasks' }, { text: 'Help' }],  
-            ],
-            resize_keyboard: true,
-            one_time_keyboard: true
+            inline_keyboard: [
+                [{ text: 'Add task', callback_data: 'add_task' }],
+                [{ text: 'Task list', callback_data: 'task_list' }, { text: 'Delete task', callback_data: 'delete_task' }],
+                [{ text: 'Delete all tasks', callback_data: 'delete_all_tasks' }, { text: 'Help', callback_data: 'help' }],
+                [{ text: 'Back', callback_data: 'back_to_menu' }]
+            ]
         }
     };
 }
 
-bot.onText(/\/start/, (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Bot is live! Choose your option:', createMainMenu());
-});
+let waitingForInput = false;
 
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    const text = msg.text;
+bot.on('callback_query', async (callbackQuery) => {
+    const chatId = callbackQuery.message.chat.id;
+    const messageId = callbackQuery.message.message_id;
+    const action = callbackQuery.data;
+
+    bot.answerCallbackQuery(callbackQuery.id);
 
     if (waitingForInput) {
-
+        bot.editMessageText('You are currently in input action(creating task), pls end it before continuing.', {
+            chat_id: chatId,
+            message_id: messageId,
+            reply_markup: createInlineMenu().reply_markup
+        });
         return;
     }
 
-    if (text === 'Add task') {
-        waitingForInput = true;
-        bot.sendMessage(chatId, 'Enter task name:');
-        bot.once('message', async (msg) => {
-            const taskName = msg.text;
-            bot.sendMessage(chatId, 'Enter your token address (tokenAddress):');
-            bot.once('message', async (msg) => {
-                const tokenAddress = msg.text;
-                const tokenSymbol = await fetchTokenSymbol(tokenAddress);
-                if (!tokenSymbol) {
-                    bot.sendMessage(chatId, 'Invalid token address. Please try again.');
-                    waitingForInput = false;
-                    return;
+    switch (action) {
+        case 'add_task':
+            waitingForInput = true;
+            bot.editMessageText('Enter task name:', {
+                chat_id: chatId,
+                message_id: messageId,
+                reply_markup: {
+                    inline_keyboard: [
+                        [{ text: 'Back', callback_data: 'back_to_menu' }]
+                    ]
                 }
-                bot.sendMessage(chatId, 'Enter MCap change:');
+            });
+
+            // Сохраняем контекст для последующих шагов
+            bot.once('message', (msg) => {
+                const taskName = msg.text;
+                bot.editMessageText('Enter your token address (tokenAddress):', {
+                    chat_id: chatId,
+                    message_id: messageId,
+                    reply_markup: {
+                        inline_keyboard: [
+                            [{ text: 'back', callback_data: 'back_to_menu' }]
+                        ]
+                    }
+                });
+
                 bot.once('message', async (msg) => {
-                    const targetMCapChange = parseFloat(msg.text);
-                    waitingForInput = false;
-                    if (isNaN(targetMCapChange)) {
-                        return bot.sendMessage(chatId, 'Pls enter valid value for MCap change.');
+                    const tokenAddress = msg.text;
+                    const tokenSymbol = await fetchTokenSymbol(tokenAddress);
+                    
+                    if (!tokenSymbol) {
+                        bot.editMessageText('Wrong token address. Please, try again.', {
+                            chat_id: chatId,
+                            message_id: messageId,
+                            reply_markup: createInlineMenu().reply_markup
+                        });
+                        waitingForInput = false;
+                        return;
                     }
-                    if (!userTasks[chatId]) {
-                        userTasks[chatId] = [];
-                    }
-                    userTasks[chatId].push({ taskName, tokenAddress, tokenSymbol, targetMCapChange, chatId, lastMCap: 0 });
-                    console.log('Текущие задачи:', userTasks);
-                    bot.sendMessage(chatId, `Task added: ${taskName} (${tokenAddress}) with target MCap change: ${targetMCapChange}`);
-                    startTrackingTasks();
+
+                    bot.editMessageText('Enter MCap change:', {
+                        chat_id: chatId,
+                        message_id: messageId,
+                        reply_markup: {
+                            inline_keyboard: [
+                                [{ text: 'Back', callback_data: 'back_to_menu' }]
+                            ]
+                        }
+                    });
+
+                    bot.once('message', async (msg) => {
+                        const targetMCapChange = parseFloat(msg.text);
+                        
+                        if (isNaN(targetMCapChange)) {
+                            bot.editMessageText('Pls, enter correct MCap change.', {
+                                chat_id: chatId,
+                                message_id: messageId,
+                                reply_markup: createInlineMenu().reply_markup
+                            });
+                            waitingForInput = false;
+                            return;
+                        }
+
+                        if (!userTasks[chatId]) {
+                            userTasks[chatId] = [];
+                        }
+
+                        userTasks[chatId].push({ 
+                            taskName, 
+                            tokenAddress, 
+                            tokenSymbol, 
+                            targetMCapChange, 
+                            chatId, 
+                            lastMCap: 0 
+                        });
+
+                        bot.editMessageText(`Task added: ${taskName} (${tokenAddress}) with MCap change: ${targetMCapChange}`, {
+                            chat_id: chatId,
+                            message_id: messageId,
+                            reply_markup: createInlineMenu().reply_markup
+                        });
+
+                        waitingForInput = false;
+                        startTrackingTasks();
+                    });
                 });
             });
-        });
-    }
-         else if (text === 'Task list') {
-        if (!userTasks[chatId] || userTasks[chatId].length === 0) {
-            return bot.sendMessage(chatId, 'No active tasks yet.');
-        }
-        const taskList = userTasks[chatId].map((task, index) => `${index + 1}. ${task.taskName} - ${task.tokenAddress} - $${task.tokenSymbol} - Target change: ${task.targetMCapChange}`).join('\n');
-        bot.sendMessage(chatId, `Active tasks:\n${taskList}`);    
-        } else if (text === 'Delete task') {
-        if (!userTasks[chatId] || userTasks[chatId].length === 0) {
-            return bot.sendMessage(chatId, 'No active tasks to delete.');
-        }
-        bot.sendMessage(chatId, 'Enter number of task to be deleted:');
-        bot.once('message', (msg) => {
-            const taskIndex = parseInt(msg.text) - 1;
-            if (taskIndex >= 0 && taskIndex < userTasks[chatId].length) {
-                userTasks[chatId].splice(taskIndex, 1); 
-                bot.sendMessage(chatId, 'Task deleted.');
-            } else {
-                bot.sendMessage(chatId, 'Incorrect task number');
+            break;
+
+        case 'task_list':
+            if (!userTasks[chatId] || userTasks[chatId].length === 0) {
+                bot.editMessageText('No active tasks.', {
+                    chat_id: chatId,
+                    message_id: messageId,
+                    reply_markup: createInlineMenu().reply_markup
+                });
+                return;
             }
-        });
-    } else if (text === 'Delete all tasks') {
-        userTasks[chatId] = [];
-        bot.sendMessage(chatId, 'All tasks have been deleted.');
-    } else if (text === 'Help') {
-        bot.sendMessage(chatId, 'Available options:\nAdd task - Add new task\nTask list - Active task list\nDelete task - Delete task\nDelete all tasks - Delete all tasks\nHelp - Help\njjuzyp.gitbook.io/logovopricetrackerbot-guide');
-}});
+            
+            const taskList = userTasks[chatId].map((task, index) => 
+                `${index + 1}. ${task.taskName} - ${task.tokenAddress} - $${task.tokenSymbol} - Целевое изменение: ${task.targetMCapChange}`
+            ).join('\n');
+            
+            bot.editMessageText(`Active tasks:\n${taskList}`, {
+                chat_id: chatId,
+                message_id: messageId,
+                reply_markup: createInlineMenu().reply_markup
+            });
+            break;
+
+        case 'delete_task':
+            if (!userTasks[chatId] || userTasks[chatId].length === 0) {
+                bot.editMessageText('No tasks to delete.', {
+                    chat_id: chatId,
+                    message_id: messageId,
+                    reply_markup: createInlineMenu().reply_markup
+                });
+                return;
+            }
+
+            bot.editMessageText('Enter task number to delete:', {
+                chat_id: chatId,
+                message_id: messageId,
+                reply_markup: {
+                    inline_keyboard: [
+                        [{ text: 'Back', callback_data: 'back_to_menu' }]
+                    ]
+                }
+            });
+
+            bot.once('message', (msg) => {
+                const taskIndex = parseInt(msg.text) - 1;
+                if (taskIndex >= 0 && taskIndex < userTasks[chatId].length) {
+                    userTasks[chatId].splice(taskIndex, 1);
+                    bot.editMessageText('Задача удалена.', {
+                        chat_id: chatId,
+                        message_id: messageId,
+                        reply_markup: createInlineMenu().reply_markup
+                    });
+                } else {
+                    bot.editMessageText('Incorrect task number.', {
+                        chat_id: chatId,
+                        message_id: messageId,
+                        reply_markup: createInlineMenu().reply_markup
+                    });
+                }
+            });
+            break;
+
+        case 'delete_all_tasks':
+            userTasks[chatId] = [];
+            bot.editMessageText('All tasks have been deleted.', {
+                chat_id: chatId,
+                message_id: messageId,
+                reply_markup: createInlineMenu().reply_markup
+            });
+            break;
+
+        case 'help':
+            bot.editMessageText('Avaliable options:\n- Add task - Add new task\n- Task list - Active task list\n- Delete task - Delete 1 task\n- Delete all tasks - Delete all existing tasks\n- Help - Help\n- jjuzyp.gitbook.io/logovopricetrackerbot-guide', {
+                chat_id: chatId,
+                message_id: messageId,
+                reply_markup: createInlineMenu().reply_markup
+            });
+            break;
+
+        case 'back_to_menu':
+            bot.editMessageText('Wellcome to LogovoPriceTrackerBot, this bot tracks solana token price changes! For more information use "Help" button! Choose your option:', {
+                chat_id: chatId,
+                message_id: messageId,
+                reply_markup: createInlineMenu().reply_markup
+            });
+            waitingForInput = false;
+            break;
+
+        default:
+            bot.editMessageText('Неизвестное действие.', {
+                chat_id: chatId,
+                message_id: messageId,
+                reply_markup: createInlineMenu().reply_markup
+            });
+            break;
+    }
+});
+
 
 async function fetchCirculatingSupply(tokenAddress) {
     try {
-        const response = await fetch(`https://api.solana.fm/v1/tokens/${tokenAddress}/supply`);
+        const response = await fetch(RPC, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                jsonrpc: "2.0",
+                id: 1,
+                method: "getTokenSupply",
+                params: [tokenAddress]
+            })
+        });
+
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
+
         const data = await response.json();
-        return data.realCirculatingSupply || 1_000_000_000; // Возвращаем реальное предложение или 1 миллиард по умолчанию
+        console.log(data.result.value.uiAmountString);
+        return data.result.value.uiAmountString || 1_000_000_000; // returning amount or 1 billion as a default for shitcoin
     } catch (error) {
         console.error('Error fetching circulating supply:', error);
         return null;
@@ -130,11 +283,11 @@ async function fetchCirculatingSupply(tokenAddress) {
 async function fetchData(task) {
     const { tokenAddress, targetMCapChange, chatId, lastMCap, taskName, tokenSymbol } = task;
 
+    console.log(`Fetching data for task: ${task.taskName}`);
+
     try {
-        if (circulatingSupply === 0) {
-            circulatingSupply = await fetchCirculatingSupply(tokenAddress);
-            if (circulatingSupply === null) return;
-        }
+
+        circulatingSupply = await fetchCirculatingSupply(tokenAddress);
 
         const response = await fetch(`https://api.jup.ag/price/v2?ids=${tokenAddress}&onlyDirectRoutes=true`);
         if (!response.ok) {
@@ -157,14 +310,23 @@ async function fetchData(task) {
     }
 }
 
+let trackingStarted = false;
+
 function startTrackingTasks() {
-    setInterval(() => {
+    
+    if (trackingStarted) return;
+    trackingStarted = true;
+    
+    setInterval(async () => {
         for (const chatId in userTasks) {
             if (userTasks[chatId]) {
-                userTasks[chatId].forEach(task => fetchData(task));
+                for (const task of userTasks[chatId]) {
+                    await fetchData(task);
+                    await new Promise(resolve => setTimeout(resolve, 500)); // 0.5 before next task update
+                }
             }
         }
-    }, 10000); // раз в 10 секунд фетч
+    }, 10000); // fetching every 10 seconds
 }
 
 bot.onText(/\/help/, (msg) => {
